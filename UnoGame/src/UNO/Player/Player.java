998c1fc3c615a4b6d4d2f7e6f86cd4d90fd0d096
paulobavr <134324556+paulobavr@users.noreply.github.com>
2023-07-03 00:17:38 +0100

    update 3

--- UnoGame/src/UNO/Player/Player.java
+++ UnoGame/src/UNO/Player/Player.java
@@ -1,16 +1,19 @@
 package UNO.Player;
 
-public class Player implements Runnable{
+public class Player { // implements Runnable
 
     private String name;
+    // isAlive
     
     public Player(String name) {
             this.name = name;
     }
 
-    //method drawcard
+    //method drawcard from deck
 
-    //Show 
+    //Show available cards
+
+    //Show possible cards to play  given the card in game
 
     //method playCard
     // Should include the message "uno"
