876637f4ffa4949b266ff588fcabfb5643c89aed
Gonçalo <goncaloscravo@gmail.com>
2023-07-05 18:14:03 +0100

    add validate card feature, fix some create username bugs, create broadcast feature, messages!!

---
+++ UnoGame/src/messages/Messages.java
@@ -0,0 +1,7 @@
+package messages;
+
+public class Messages {
+
+    public static final String YOUR_TURN = " IT'S YOUR TURN!";
+    public static final String WAIT_TURN = " IS PLAYING, WAIT ...";
+}
